4c2c0d5125e970991f976cc106a5e5361f92c2fc
naleksankins <naleksankins@evolutiongaming.com>
2020-04-27 21:28:35 +0300

    Update remove first product from cart method. Add assertion that product is removed.

--- test-case/src/main/java/CartPage.java
+++ test-case/src/main/java/CartPage.java
@@ -1,9 +1,15 @@
-import static com.codeborne.selenide.Selenide.$;
+import lombok.val;
+import org.testng.Assert;
+
+import static com.codeborne.selenide.Selenide.*;
 
 public class CartPage extends CommonPage {
     public void removeFirstCart() {
-        $("li:nth-child(1) > div > div.product__info__part.product__controls.product__controls--small > a")
-                .click();
+        val elementSelector = "li:nth-child(1) > div > div:nth-child(1) > h3 > a";
+        val elementToRevome = $(elementSelector).getText();
+        $("li:nth-child(1) > div > div.product__info__part.product__controls.product__controls--small > a").click();
+        val currentElement = $(elementSelector).getText();
+        Assert.assertNotEquals(currentElement, elementToRevome, "Element not deleted from cart");
     }
 
 }
